55cc12da3a54bbc098af01e3667c665cf565625a
norbix93 <norbix93@gmail.com>
2019-05-07 14:02:53 +0200

    change text

--- src/test/java/at/nacs/devopscontinuousintegration/MessageEndpointTest.java
+++ src/test/java/at/nacs/devopscontinuousintegration/MessageEndpointTest.java
@@ -19,7 +19,7 @@ class MessageEndpointTest {
     @Test
     void get() {
         String actual = restTemplate.getForObject(url, String.class);
-        String expected = "I am living the Dream!";
+        String expected = "I am living the dream!";
         assertEquals(expected, actual);
     }
 }
